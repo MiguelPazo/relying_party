daee97ec9fe03de64454ae976306c087d932a225
Miguel Pazo <miguel.ps19@gmial.com>
2016-09-29 12:40:07 -0500

    test change storegae

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,6 +5,8 @@
         <div class="row">
             <div class="col-sm-12">
                 <h1>[[ data ]]</h1>
+
+                <h2>Bienvenido [[ user.nam ]]</h2>
                 <a ng-click="logout()" class="btn btn-lg btn-danger">
                     Logout
                 </a>
